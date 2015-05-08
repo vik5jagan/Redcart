f4bc7acbf03a16363ad24ebc90b98997dc122910
vikjagan <vikjagan@gmail.com>
2015-05-07 19:08:40 -0700

    Added code to make server side call to Parse.com for new user signup and signin.

--- app/app.js
+++ app/app.js
@@ -10,3 +10,5 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/landing'});
 }]);
+
+Parse.initialize("oh59grSAAuNE0ZdcrHCAs6lHX0uf9PyhfYGqwBtp", "VDOLSHeddf8HAl1RzpiJv97OXdWO74Ea314s7Jrz");
--- app/landing/landing.js
+++ app/landing/landing.js
@@ -2,21 +2,22 @@
 
 angular.module('myApp.landing', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/landing', {
-    templateUrl: 'landing/landing.html',
-    controller: 'LandingCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
 
-    .controller('LandingCtrl', ['$scope','$location',function($scope,$location) {
+        $routeProvider.when('/landing', {
+          templateUrl: 'landing/landing.html',
+          controller: 'LandingCtrl'
+        });
+
+    }])
+
+    .controller('LandingCtrl', ['$scope','$location','$sce',function($scope,$location, $sce) {
 
       console.log("Invoked the LandingCtrl:");
       $scope.user = {};
 
       $scope.init = function () {
         $scope.selection = "signin";
-
       };
 
       $scope.init();
@@ -29,10 +30,43 @@ angular.module('myApp.landing', ['ngRoute'])
         console.log("Here is the email :"+email);
         console.log("Here is the password :"+password);
 
+        var Identity = Parse.Object.extend("Identity");
+        var query = new Parse.Query(Identity);
+        query.equalTo("email", email);
+        query.find({
+          success: function(results) {
+            alert("Successfully retrieved " + results.length + " scores.");
+            // Do something with the returned Parse.Object values
+            var serverPwd = "";
+            for (var i = 0; i < results.length; i++) {
+              var object = results[i];
+              alert(object.id + ' - ' + object.get('password'));
+               serverPwd = object.get('password');
+            }
+              if(password == serverPwd){
+                $scope.$apply(function() {
+                   $location.path('/home');
+                });
+              }else{
+                $scope.$apply(function() {
+                  $scope.formSigninMsg = $sce.trustAsHtml('<div class="alert alert-warning">Password not matching</div>');
+                });
+              }
+
+          },
+          error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+            $scope.$apply(function() {
+                $scope.formSigninMsg = $sce.trustAsHtml('<div class="alert alert-warning">Following error happened in server.'+error.code+':'+error.message+' </div>');
+            });
+          }
+        });
+
+
         if(email == password){
 
           console.log("Login happened successfully!!!");
-          $location.path('/home');
+
 
         }else{
 
@@ -42,6 +76,7 @@ angular.module('myApp.landing', ['ngRoute'])
 
       };
 
+
       $scope.gotoSignup = function (e){
 
         console.log("signup button c1lick3ed !!!");
@@ -49,11 +84,43 @@ angular.module('myApp.landing', ['ngRoute'])
         $scope.selection = "signup";
       };
 
+
+
       $scope.signup = function (e){
 
         console.log("signup button c1licked !!!");
 
-        $scope.selection = "signin";
+        var email = $scope.user.email;
+        var password = $scope.user.password;
+
+        var Identity = Parse.Object.extend("Identity");
+        var identity = new Identity();
+        identity.set("email",email);
+        identity.set("password",password);
+        var message ="";
+
+        identity.save(null,{
+
+          success: function(object){
+
+            console.log("data submitted !!!");
+            $scope.$apply(function() {
+
+              $scope.formMsg = $sce.trustAsHtml('<div class="alert alert-success fade in">This is a success message</div>');
+
+            });
+          },
+          error: function(model,error){
+           console.log(error);
+            $scope.$apply(function() {
+
+              $scope.formMsg = $sce.trustAsHtml('<div class="alert alert-warning">Not able to signup!!</div>');
+            });
+          }
+        });
+
+       // $scope.selection = "signin";
+
       }
 
 
